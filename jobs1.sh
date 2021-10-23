112bcc80ef7e0e361317dd8ecef27b74289a3805
sidharthkumar10500 <sidharthpunia10500@gmail.com>
2021-10-23 15:50:52 -0500

    added the bash script to run the data gen file iteratibely

---
+++ jobs1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for target_scan in 0 1 2 3 4 5 6 7 8 9 10
+do
+    for mini_contrast in 't2' 't1' #10 11 13
+    do
+        python realnoisedata_gen.py -m mini_contrast -t target_scan 
+    done
+done
+echo "All done"
