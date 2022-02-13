374118ca23dd975e3dca8a20748215a616fc3c90
sidharthkumar10500 <sidharthpunia10500@gmail.com>
2022-02-13 16:52:33 -0600

    running for different slices

--- data_preprocessing/motion_gen_jobs.sh
+++ data_preprocessing/motion_gen_jobs.sh
@@ -6,7 +6,7 @@ export TE_bw_echoes=10
 for subject in {12..27}
 do  
     echo "starting subject $subject "
-    for slice in {15..35}
+    for slice in {20..40}
     do 
         echo "starting slice $slice"
         python motion_gen_FSE.py -sb $subject -sl $slice -etl $ETL -te $TE_effective -teb $TE_bw_echoes
