aac575dde3e37bd2f19fbffe0b83cdcd54ad5836
sidharthkumar10500 <sidharthpunia10500@gmail.com>
2022-02-26 19:05:01 -0600

    changed the hyper parameters for training

--- bash_training_jobs.sh
+++ bash_training_jobs.sh
@@ -12,11 +12,16 @@
 
 export data_file='repo_text_files'
 
-python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 1 -sc FSE -df ${data_file}
-python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.1 -sc FSE -df ${data_file}
-python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.5 -sc FSE -df ${data_file}
-python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 0.1 -sc FSE -df ${data_file}
+python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 5 -B 20 -lb 1 -lb_b 0.1 -sc SE -df ${data_file}
+python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 5 -B 20 -lb 0.1 -lb_b 0.1 -sc SE -df ${data_file}
+# python train_main.py -e 20 -ma GAN -G 1 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.1 -sc SE -df ${data_file}
+# python train_main.py -e 20 -ma GAN -G 2 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 1 -sc SE -df ${data_file}
 
-python train_main.py -e 20 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 1 -sc FSE -df ${data_file}
-python train_main.py -e 20 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.1 -sc FSE -df ${data_file}
+# python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 1 -sc FSE -df ${data_file}
+# python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.1 -sc FSE -df ${data_file}
+# python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.5 -sc FSE -df ${data_file}
+# python train_main.py -e 10 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 0.1 -sc FSE -df ${data_file}
+
+# python train_main.py -e 20 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 1 -sc FSE -df ${data_file}
+# python train_main.py -e 20 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 1 -lb_b 0.1 -sc FSE -df ${data_file}
 # python train_main.py -e 20 -ma GAN -G 0 -de 20 -ge 10 -B 20 -lb 0.1 -lb_b 1 -sc SE -df ${data_file}
